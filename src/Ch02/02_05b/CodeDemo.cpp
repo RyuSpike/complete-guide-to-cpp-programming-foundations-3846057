1442964590c83fbc25904a551bd9ca83435bc86e
Noah Maseko <72697632+RyuSpike@users.noreply.github.com>
2026-01-15 14:42:04 +0000

    Variables there is a bug for int a,b = 5. It assigns a a value of 0.

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,19 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
+int i_am_a_global_variable = 4; //best practice is to avoid using these.
 
 int main(){
-    
+    int a = 7; 
+    int b = 7;
+    int c = a * b; //local variables
+    float d = a/b;
+    bool flag = true;
+    std::cout << "a = " << a << std::endl 
+    << "b = " << b << std::endl <<
+    "c = " << c << std::endl << 
+    "d = " << d << std::endl << flag << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
