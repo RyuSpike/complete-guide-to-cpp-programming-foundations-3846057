3e71b1055005d8b1098ad872128dcf78ae9a07c5
Noah Maseko <72697632+RyuSpike@users.noreply.github.com>
2026-01-15 16:22:25 +0000

    Example of a structure also use enum classes

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,22 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character{
+    std::string name;
+    int level;
+    character_role role;
+};
+
 int main(){
     
+    game_character buddy;
+    buddy.name = "Tommy Vercetti";
+    buddy.level = 999;
+    buddy.role = character_role::protagonist;
+
+    std::cout << buddy.name << " is a level " << buddy.level << " at the end of GTA Vice City if it had exp points lol. his role is " <<  (int) buddy.role << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
