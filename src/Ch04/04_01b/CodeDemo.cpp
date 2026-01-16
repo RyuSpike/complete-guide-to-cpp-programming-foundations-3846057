c07cd4cef0923f17af8475112f867750943967df
Noah Maseko <72697632+RyuSpike@users.noreply.github.com>
2026-01-16 12:28:01 +0000

    Good stuff

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,17 @@
 #include <iostream>
 
 int main(){
+    int enemies[4];
     
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+    
+    std::cout << "The game has 4 levels:" << std::endl;
+    for(int i =0;i<4;i++){
+      std::cout << "Level " << i << ": " << enemies[i] << std::endl;  
+    }
     std::cout << std::endl << std::endl;
     return 0;
 }
